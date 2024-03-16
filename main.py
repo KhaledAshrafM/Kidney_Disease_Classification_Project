4461b8e25147d56110e4497ee8b8bebb11859296
khaled ashraf <114561476+KhaledAshrafM@users.noreply.github.com>
2024-03-16 22:26:55 +0200

    model training added

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_model_base import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
-
-STAGE_NAME = "Data Ingestion stage"
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        data_ingestion = DataIngestionTrainingPipeline()
-        data_ingestion.main()
-        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\nnx=======x")
-    except Exception as e:
-        logger.exception(e)
-        raise e
+# STAGE_NAME = "Data Ingestion stage"
+# if __name__ == '__main__':
+#     try:
+#         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+#         data_ingestion = DataIngestionTrainingPipeline()
+#         data_ingestion.main()
+#         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\nnx=======x")
+#     except Exception as e:
+#         logger.exception(e)
+#         raise e
     
 
 STAGE_NAME = "Prepare base model"
@@ -24,4 +24,15 @@ try:
     prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
 except Exception as e:
+    raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_training = ModelTrainingPipeline()
+    model_training.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
     raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,9 @@
 from src.cnnClassifier.constants import *
-from src.cnnClassifier.utils.common import read_yaml, create_directories
+import os
+from src.cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from src.cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                    PrepareBaseModelConfig)
+                                                PrepareBaseModelConfig,
+                                                TrainingConfig)
 
 
 class ConfigurationManager:
@@ -15,6 +17,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+
     
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -25,12 +28,14 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir
+            unzip_dir=config.unzip_dir 
         )
 
         return data_ingestion_config
+    
 
 
+    
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
         
@@ -48,3 +53,28 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney_ct_scan_image")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
